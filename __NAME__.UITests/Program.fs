116e16eb5ac4b22dcded9eb4d6b0b36e15e81e37
Amir Rajan <ar@amirrajan.net>
2012-07-28 09:01:59 -0500

    updated ui automation references

--- __NAME__.UITests/Program.fs
+++ __NAME__.UITests/Program.fs
@@ -1,2 +1,22 @@
-﻿// Learn more about F# at http://fsharp.net
+﻿open runner
 
+open canopy
+
+start "firefox"
+
+let reset _ =
+    let seed = new Oak.Controllers.SeedController()
+    seed.PurgeDb() |> ignore
+    seed.All() |> ignore
+    () 
+
+before <- reset
+
+test(fun _ ->
+    describe "hello world"
+    url "http://localhost:3000/"
+)
+
+run()
+
+quit()
